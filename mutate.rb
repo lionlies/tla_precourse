3d5375ad5552bb0c43adaf1edbdacf70cb5a77e9
Jason Lung <lionlies@gmail.com>
2014-08-19 21:37:28 -0400

    use p method to print out no_mutate(a)

--- mutate.rb
+++ mutate.rb
@@ -1,11 +1,11 @@
 a = [1, 2, 3]
 
 # def mutate(array)
-#   array.pop
+#  array.pop
 # end
 
 # p "Before mutate method: #{a}"
-# mutate(a)
+# p mutate(a)
 # p "After mutate method: #{a}"
 
 def no_mutate(array)
@@ -13,5 +13,5 @@ def no_mutate(array)
 end
 
 p "Before no_mutate method: #{a}"
-no_mutate(a)
+p no_mutate(a)
 p "After no_mutate method: #{a}"
